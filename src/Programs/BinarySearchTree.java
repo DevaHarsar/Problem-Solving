e7ecb6f94495aa334d0b38c129d9634995918996
Deva Harsar M M <72179089+DevaHarsar@users.noreply.github.com>
2025-05-13 16:22:10 +0530

    Programms

--- src/Programs/BinarySearchTree.java
+++ src/Programs/BinarySearchTree.java
@@ -19,10 +19,11 @@ public class BinarySearchTree {
              return new Node(key);
         if(root.data==key)
              return root;
-        else if(root.data<key)
-            root.left=insert(root.left,key);
+        else if (key < root.data)
+            root.left = insert(root.left, key);
         else
-            root.right=insert(root.right,key);
+            root.right = insert(root.right, key);
+
 
         return root;
 
@@ -52,9 +53,9 @@ public class BinarySearchTree {
     {
         if(root!=null)
         {
-            System.out.print(root.data+" ");
             postorder(root.left);
             postorder(root.right);
+            System.out.print(root.data+" ");
         }
 //        System.out.println();
 
@@ -69,10 +70,24 @@ public class BinarySearchTree {
     public static int depth(Node root,int x)
     {
         if(root==null) return -1;
-        int dist=-1;
-        if((root.data==x )|| (dist=depth(root.left,x))>=0|| (dist=depth(root.right,x))>=0)
-             return dist+1;
-        return dist;
+//        int dist=-1;
+//        if((root.data==x )|| (dist=depth(root.left,x))>=0|| (dist=depth(root.right,x))>=0)
+//             return dist+1;
+//        return dist;
+        int d=-1;
+        while(root!=null)
+        {
+            if(root.data==x) return d;
+
+            if(root.data<x)
+                root=root.right;
+            else
+                root=root.left;
+
+            d++;
+
+        }
+        return d;
     }
 
 //        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
@@ -86,6 +101,48 @@ public class BinarySearchTree {
 //            else return right;
 //        } ansestor problems
 
+    public static Node delnode(Node root,int key)
+    {
+        if(root==null) return root;
+        if(root.data>key)
+             root.left=delnode(root.left,key);
+        else if(root.data<key)
+             root.right=delnode(root.right,key);
+        else {
+            if(root.left==null)
+                 return root.right;
+            else if(root.right==null)
+                return root.left;
+            Node succ=getsucc(root);
+            root.data=succ.data;
+            root.right=delnode(root.right,succ.data);
+
+        }
+            return root;
+    }
+    public static Node getsucc(Node root)
+    {
+        Node curr=root.right;
+        while(curr!=null && curr.left!=null)
+        {
+            curr=curr.left;
+        }
+        return curr;
+    }
+    int diameter;
+    public int diameterOfBinaryTree(Node root) {
+        diameter=0;
+        help(root);
+        return diameter;
+    }
+    public int help(Node root)
+    {
+        if(root==null) return 0;
+        int l=help(root.left);
+        int r=help(root.right);
+        diameter=Math.max(diameter,l+r);
+        return Math.max(l,r)+1;
+    }
 
     public static void main(String[] args) {
         Node root=null;
@@ -101,6 +158,10 @@ public class BinarySearchTree {
 
         System.out.println("Height is "+height(root));
         System.out.println("Depth is "+depth(root,15));
+
+        root= delnode(root,10);
+        System.out.println("Deletion of the node next");
+        inorder(root);
 
     }
 }
